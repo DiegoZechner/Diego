26e02f72841f4b1678f396a2d3e3df858b50c9a2
Diego <you@example.com>
2022-04-06 14:42:40 +0200

    zh890ß

---
+++ src/IF_Bedingung_Aufgabe_1.java
@@ -0,0 +1,22 @@
+
+import java.util.Random;
+
+public class IF_Bedingung_Aufgabe_1 {
+    public static void main(String[]args) {
+
+        Random random =new Random();
+        int randomNumber = random.nextInt(100);
+
+        if((randomNumber>1) && (randomNumber<20)){
+            System.out.println("Mini");
+
+        }else if ((randomNumber>20) &&(randomNumber<50)){
+            System.out.println("Medium");
+        }else{
+            System.out.println("Large");
+        }
+    }
+
+
+
+}
