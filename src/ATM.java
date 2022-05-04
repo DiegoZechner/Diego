43d3bfb5b450b6c7e68e38d9c9b25a50a5fcebb6
Diego <you@example.com>
2022-05-04 15:38:23 +0200

    ATM Fertig

--- src/ATM.java
+++ src/ATM.java
@@ -6,10 +6,11 @@ public class ATM {
         Scanner sc = new Scanner(System.in);
         while(true)
         {
-            System.out.println("Welcome to your ATM of trust");
+            System.out.println("Welcome to your ATM of trust <3");
             System.out.println("1.Withdraw");
             System.out.println("2.Deposite");
             System.out.println("3.Check Balance");
+            System.out.println("4.LEAVE");
             num = sc.nextInt();
 
             switch (num)
@@ -20,11 +21,11 @@ public class ATM {
                     if(balance>=withdraw)
                     {
                         balance = balance-withdraw;
-                        System.out.println("Balance Amount is:"+balance);
+                        System.out.println("Balance Amount is: 11"+balance);
                         System.out.println("Please collect money");
                     }
                     else{
-                        System.out.println("You do not have that much money, to withdraw");
+                        System.out.println("You do not have that much money, to withdraw.");
                     }
                     System.out.println(" ");
                     break;
@@ -33,6 +34,17 @@ public class ATM {
                     deposite = sc.nextInt();
                     balance = balance+deposite;
                     System.out.println("Balance Ampunt is: "+balance);
+                    System.out.println("    ");
+                    break;
+                case 3:
+                    System.out.println("Balance of my Account is:   "+balance);
+                    System.out.println("    ");
+                    break;
+                case 4:
+                    System.exit(0);
+                    System.out.println("    ");
+
+
 
             }
 
