a5d631baae7229ec49876587dd2469fccad8d465
Diego <you@example.com>
2022-09-16 14:48:44 +0200

    Array_1

---
+++ src/Arrays/Arrays_1.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Arrays_1 {
+    public static void main(String[]args){
+
+        int[] userInput = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 0; i < userInput.length; i++) {
+            System.out.println("Enter number, amount:" + i);
+            userInput[i] = scanner.nextInt();
+            {
+                sum += userInput[i];
+            }
+        }
+        System.out.println("Result of the 10 numbers is:" + sum);
+    }
+}
