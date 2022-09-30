5b47ef8bf80b547bc0108ea4335629d037ca1992
Diego <you@example.com>
2022-09-30 14:44:18 +0200

    Caesar fertig

--- src/Arrays/caesar.java
+++ src/Arrays/caesar.java
@@ -1,7 +1,35 @@
 package Arrays;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
 
-public class caesar{
+public class caesar {
 
-    private int switchIt = -1;
-    private char[] chArray = null;
+    public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+        Random rn = new Random();
+
+        System.out.println("Geben Sie gewünschtes Wort ein!");
+
+        String textInput = scanner.next();
+
+        int offset = rn.nextInt(2,4);
+
+        char string[] = textInput.toCharArray();
+
+        for(int i = 0; i<string.length; i++){
+            char text = string[i];
+             text += offset;
+            string[i] = text;
+
+        }
+        System.out.println("");
+        System.out.println("um so viele Stellen verändert: " + offset);
+        System.out.println("");
+        System.out.println(string);
+        System.out.println("");
+        System.out.println("original: " + textInput);
+
+    }
+}
