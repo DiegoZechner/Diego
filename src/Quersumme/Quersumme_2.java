5fff0a25afa7aa07486e5972cfc1eda4e2a49696
Diego <you@example.com>
2022-10-07 14:28:37 +0200

    Quersumme 2 Fertig

---
+++ src/Quersumme/Quersumme_2.java
@@ -0,0 +1,24 @@
+package Quersumme;
+
+public class Quersumme_2{
+
+
+    public static void main(String[] args){
+
+        for (int cnt = 0; cnt < 1000; cnt++) {
+            String numberAsString = Integer.toString(cnt);
+            char[] numbers = numberAsString.toCharArray();
+
+            int sum = 0;
+            for (int i = 0; i < numbers.length ; i++) {
+                sum += Character.getNumericValue(numbers[i]);
+
+            }
+            int rest = sum % 7;
+
+            if(rest ==0){
+                System.out.println("Number:"+ cnt + "Quersumme:" + sum);
+            }
+        }
+    }
+}
