041d8b3666d4e94817a9d09aa865c38985b29dcf
Diego <you@example.com>
2023-04-11 11:30:27 +0200

    Foto machen

--- src/at/dz/oo/Camera/Camera.java
+++ src/at/dz/oo/Camera/Camera.java
@@ -2,38 +2,54 @@ package at.dz.oo.Camera;
 
 import java.util.Scanner;
 
+
+
 public class Camera {
     private int resolutionLevel;
+    private int name;
+    private int counter;
     Scanner scan = new Scanner(System.in);
 
+    public Camera(){
+        this.counter = 999;
+        this.name = this.counter;
+    }
 
-    public void setResolutionLevel() {
+    public Storage setResolutionLevel() {
+        System.out.println("Bitte geben Sie die Auflösungsstufe ein (1-3):");
         int level = scan.nextInt();
         switch (level) {
             case 1:
                 this.resolutionLevel = 1;
-                System.out.println("Low - 2GB");
+                System.out.println("Niedrig - 2GB");
                 break;
             case 2:
                 this.resolutionLevel = 2;
-                System.out.println("Medium - 4GB)");
+                System.out.println("Mittel - 4GB");
                 break;
             case 3:
                 this.resolutionLevel = 3;
-                System.out.println("High - 6GB");
+                System.out.println("Hoch - 6GB");
                 break;
             default:
-                System.out.println("No available resolution level");
+                System.out.println("Keine gültige Auflösungsstufe");
         }
+        return null;
     }
 
-    public void takePicture(int resolutionLevel, String name){
-        int
+
+
+    public void takePicture() {
+        this.counter++;
+        this.name = this.counter;
+        System.out.println("Bild mit Auflösungsstufe " + resolutionLevel + " aufgenommen " + name + " - jpg");
     }
+
     public int getResolutionLevel() {
         return this.resolutionLevel;
     }
-    public void size(){
+
+    public void size() {
 
     }
 
--- src/at/dz/oo/Camera/Producer.java
+++
@@ -1,4 +0,0 @@
-package at.dz.oo.Camera;
-
-public class Producer {
-}
